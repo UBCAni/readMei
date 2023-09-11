3ac11725eb34f9f8df05de84cb19b57bece989f3
poff <jgyzeng@gmail.com>
2023-09-11 05:28:19 -0700

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Message";
+
+function App() {
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name = 'poff';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
