c8c75d6440905f87592509b2e12d96b0b1c3e044
najzeko <markom@novuscom.net>
2023-10-28 21:44:37 -0700

    connvert input to number for api call

--- frontend/src/get-member/components/index/container.tsx
+++ frontend/src/get-member/components/index/container.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement, useState } from "react";
 import { InputEvent } from "../../../common/interfaces.ts";
 import GetMember from "./get-member.tsx";
-import {GetMemberResponse, GetMemberError} from "./interfaces.ts";
+import { GetMemberResponse, GetMemberError } from "./interfaces.ts";
+import { convertInput } from "../../../common/utils/convertInput.ts";
 
 interface GetMemberContainerProps {
     getMember: (membershipNum: string) => Promise<GetMemberResponse>
@@ -27,8 +28,9 @@ const GetMemberContainer = (props:GetMemberContainerProps): ReactElement => {
     }
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
-        props.getMember(membershipNum).then((myresponse: GetMemberResponse) => {
+        
+        const strippedNum = convertInput(membershipNum)
+        props.getMember(strippedNum).then((myresponse: GetMemberResponse) => {
                 handleResponse(myresponse)
             }
         ).catch((myerror: GetMemberError) => {
