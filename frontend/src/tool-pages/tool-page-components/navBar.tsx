16c0a82e014ae779c36adb861e80c29469207ef4
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2024-04-25 01:36:20 -0700

    primary theme change

--- frontend/src/tool-pages/tool-page-components/navBar.tsx
+++ frontend/src/tool-pages/tool-page-components/navBar.tsx
@@ -3,17 +3,17 @@ import {ReactElement} from "react";
 
 const ReadMeiNavBar = (): ReactElement => {
     return <>
-      <Navbar  expand={false} className="bg-body-tertiary" fixed="top">
+      <Navbar  expand={false} className="bg-primary" fixed="top">
     <div className="w-100 justify-content-between">
-      <div className="p-2"> 
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Brand> ReadMei</Navbar.Brand>
+      <div className="p-2 className='text-light'"> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand className='text-light'> ReadMei</Navbar.Brand>
       </div>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto p-2">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/member-search">User Search</Nav.Link>
-          <Nav.Link href="/events-manage">Manage Events</Nav.Link>
+          <Nav.Link href="/" className='text-light'>Home</Nav.Link>
+          <Nav.Link href="/member-search" className='text-light'>User Search</Nav.Link>
+          <Nav.Link href="/events-manage" className='text-light'>Manage Events</Nav.Link>
           {/* <Nav.Link href="/tool-template">Tool Template</Nav.Link> */}
 
           {/* Add links to more tools as navlinks here*/}
